ca18439134f7a1de22981fe5b1eecf1d8e30d9c1
kkk <mmma@m>
2020-12-30 13:44:54 +0100

    chat bubble

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,3 +1,22 @@
+function toggleBlur(){
+    hidELem=document.getElementsByClassName('hidden');
+    for (i=0; i<2; i++){
+        if (hidELem[i].classList[1] == "blur"){
+        hidELem[i].classList.remove('blur')
+        document.getElementById('show').classList.add('toggled')
+    } else{
+        hidELem[i].classList.add('blur')
+        document.getElementById('show').classList.remove('toggled')
+    }
+    }
+    
+}
+function bottomDiv(){
+    var objDiv = document.getElementById("chatWindow");
+    objDiv.scrollTop = objDiv.scrollHeight; 
+}
+window.setInterval(bottomDiv,5000)
+
 function encryption(mess, key) {
   return CryptoJS.AES.encrypt(mess, key);
 }
@@ -5,39 +24,36 @@ function decryption(mess_ecrypted, key) {
   return CryptoJS.AES.decrypt(mess_ecrypted, key);
 }
 // load key from cookies
-$("#key").text(getCookie("key_encryption"));
+$("#roomKey").val(getCookie("key_encryption"));
 
 // new key for encryption
 $("#send_key").click(function () {
   console.log("aaa");
-  setCookie("key_encryption", $("#encrytp").val(), 2);
-  $("#key").text(getCookie("key_encryption"));
+  setCookie("key_encryption", $("#roomKey").val(), 2);
+  $("#roomKey").val(getCookie("key_encryption"));
 });
 // send and refresh chat
 function send_and_refresh() {
   var jqxhr = $.getJSON("/chat_check", function () {})
     .done(function (data) {
-      $("#chat_box").html("");
+      $("#chatWindow").html("");
       $.each(data, function () {
         if (this.user == 1) {
-          class_to = 'class="user2"';
-        } else {
           class_to = 'class="user1"';
+        } else {
+          class_to = 'class="user2"';
         }
-        // console.log(decryption(this.mess, $("#key").text(CryptoJS.enc.Utf8)) )
-        //  console.log(decryption(this.mess, $("#key").text()).toString(CryptoJS.enc.Utf8));
-        // $("#chat_box").append("<p " + class_to + " > " + decryption(this.mess, $("#key").text()).toString(CryptoJS.enc.Utf8) + " </p>")
         if (this.mess == null) {
           console.log("null");
         } else {
-          $("#chat_box").append(
+          $("#chatWindow").append(
             "<p " +
               class_to +
-              " > " +
-              decryption(this.mess, $("#key").text()).toString(
+              " > <span>" +
+              decryption(this.mess, $("#roomKey").val()).toString(
                 CryptoJS.enc.Utf8
               ) +
-              " </p>"
+              "</span> </p>"
           );
         }
       });
@@ -52,7 +68,7 @@ function send_and_refresh() {
 $(document).ready(function () {
   $("form").submit(function () {
     var $input_mess = $(this).find("input[name=chat_mess]");
-    var $input_encryption = $("#key").text();
+    var $input_encryption = $("#roomKey").val();
     if (!$input_mess.val() || !$input_encryption) {
       alert("wypełnij oba pola");
     } else {
