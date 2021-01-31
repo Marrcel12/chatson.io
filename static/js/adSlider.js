73fb21ea4367139cb4d1b635b902d4ddff11afd9
Adrian <adi.jobda@gmail.com>
2021-01-31 13:31:02 +0100

    ads mod

--- static/js/adSlider.js
+++ static/js/adSlider.js
@@ -1,16 +1,16 @@
-function changeAd(){
-    var adIndex= document.getElementById('adCounter').value
-    ads = document.getElementsByClassName('ad')
-    for (i=0;i<ads.length;i++){
-        ads[i].style.display = 'none';
-    }
-    if(adIndex == ads.length-1){
-        document.getElementById('adCounter').value = 0;
-        ads[0].style.display = 'block';
-    }else{
-        document.getElementById('adCounter').value = parseInt(adIndex)+1
-        ads[document.getElementById('adCounter').value].style.display = 'block';
-    }
-}
-
-setInterval(changeAd, 5000)
+//function changeAd(){
+//    var adIndex= document.getElementById('adCounter').value
+//    ads = document.getElementsByClassName('ad')
+//    for (i=0;i<ads.length;i++){
+//        ads[i].style.display = 'none';
+//    }
+//    if(adIndex == ads.length-1){
+//        document.getElementById('adCounter').value = 0;
+//        ads[0].style.display = 'block';
+//    }else{
+//        document.getElementById('adCounter').value = parseInt(adIndex)+1
+//        ads[document.getElementById('adCounter').value].style.display = 'block';
+//    }
+//}
+//
+//setInterval(changeAd, 5000)
