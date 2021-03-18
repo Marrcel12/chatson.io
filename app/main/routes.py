dc8d3ff7a7ba086b7159b98022b92f5e70d89d1a
Adrian <adi.jobda@gmail.com>
2021-03-18 23:55:20 +0100

    mobileToTest

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,6 +7,7 @@ def index():
     if request.method == 'POST':
         session['name'] = request.form.get("name")
         session['room'] = request.form.get("room")
+        session['res'] = request.form.get("wid")
         return redirect(url_for('.chat'))
     return render_template('index.html')
 
@@ -17,4 +18,8 @@ def chat():
     room = session.get('room', '')
     if name == '' or room == '':
         return redirect(url_for('.index'))
-    return render_template('chat.html', name=name, room=room)
+    print(session['res'])
+    if int(session["res"])>=800:
+        return render_template('chat.html', name=name, room=room)
+    else:
+        return render_template('chatMobile.html', name=name, room=room)
