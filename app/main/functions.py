4bec9e88e558a208dac082dd29a21a99aad208d2
Adrian <adi.jobda@gmail.com>
2021-03-24 23:39:13 +0100

    new routes

---
+++ app/main/functions.py
@@ -0,0 +1,83 @@
+import connectionString
+def check_if_id_not_in_base(id):
+    conn = psycopg2.connect(connectionString.connString)
+    c = conn.cursor()
+    c.execute(
+        "SELECT key FROM chat_creds  where  key=%s;", (id,))
+    rows = c.fetchall()
+    tab = []
+    for row in rows:
+        tab.append(row)
+    conn.close
+    if tab:
+        return False
+    else:
+        return True
+
+
+def pre_gen(number_of_characters):
+    propos = rooms_id_generator(number_of_characters)
+    if check_if_id_not_in_base(propos):
+        return propos
+# database function
+
+
+def check_creditional(key, password):
+    conn2 = psycopg2.connect(connectionString.connString)
+    c2 = conn2.cursor()
+    c2.execute(
+        "SELECT key FROM chat_creds where save_passwd=%s and key=%s;", (password, key))
+    if len(c2.fetchall()) == 0:
+        print("Wrong_password")
+        conn2.close()
+        return False
+    return True
+
+
+def save_chat_database(data, new_key):
+    conn = psycopg2.connect(connectionString.connString)
+    c = conn.cursor()
+    key = data["key"]
+    password = data["save_passwd"]
+    if new_key:
+        to_execute = (key, password)
+        c.execute(
+            "INSERT INTO chat_creds(key, save_passwd)  VALUES (%s, %s );", to_execute)
+        conn.commit()
+    elif not(check_creditional(key, password)):
+        return False
+    for message in data["messages"]:
+        to_execute = (message["user"],   message["text"],  key)
+        c.execute(
+            'INSERT INTO chat_mess("user", text, key)  VALUES (%s, %s, %s    );', to_execute)
+        conn.commit()
+    conn.close()
+    return True
+
+
+def load_chat_database(key, password):
+    conn = psycopg2.connect(connectionString.connString)
+    c = conn.cursor()
+    if not(check_creditional(key, password)):
+        return [False, []]
+    else:
+        to_return = []
+        c.execute("SELECT user,text FROM chat_mess where key=%s;", (key,))
+
+        for message in c.fetchall():
+            to_return.append({"user": message[0], "text": message[1]})
+
+    return [True, to_return]
+
+
+def del_chat_database(key, password):
+    conn = psycopg2.connect("dbname=chatson user=chatson password=qaz123")
+    c = conn.cursor()
+    if not(check_creditional(key, password)):
+        return False
+    c.execute("DELETE FROM chat_table WHERE key=%s;", (key,))
+    c.execute(
+        "DELETE FROM chat_creds WHERE key=%s and save_passwd=%s;", (key, password))
+    conn.commit()
+    conn.close()
+    return True
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,7 @@
 from flask import session, redirect, url_for, render_template, request
 from . import main
+import connectionString
+import functions
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -23,3 +25,34 @@ def chat():
         return render_template('chat.html', name=name, room=room)
     else:
         return render_template('chatMobile.html', name=name, room=room)
+    
+@main.route('/save_chat', methods=['POST'])
+def save_chat():
+    data = request.get_json()
+    new_key = False
+    key_return = None
+    if data["key"] == None:
+        data["key"] = functions.pre_gen(12)
+        new_key = True
+    success = functions.save_chat_database(data, new_key)
+    print("success")
+    if success:
+        key_return = data["key"]
+    return jsonify({"success": success, "key": key_return})
+
+
+@main.route('/load_chat', methods=['POST'])
+def load_chat():
+    data = request.get_json()
+    key = data["key"]
+    result = functions.load_chat_database(key, data["save_passwd"])
+    return jsonify({"success": result[0], "messages": result[1]})
+# TODO: DELfrom database
+
+
+@main.route('/del_chat', methods=['POST'])
+def del_chat():
+    data = request.get_json()
+    result = functions.del_chat_database(data["key"], data["save_passwd"])
+    return jsonify({"success": result})
+# end new
